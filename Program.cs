699759b510cc8a39a1b96eec4fb99167249f6d99
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-30 19:40:36 -0300

    add com cor na letra

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Writeline ("Aham, aham-aham, aham-aham, ayy");
+Console.Writeline ("Aham, aham-aham, aham-aham, ayy");
+Console.Writeline ("Pau no cu da concorrência, aqui é o bife, porra");
+Console.Writeline ("Ta-ra-ta-ta, ta-ra-ta-ta");
+
+Console.Writeline ("Drip veneno, desce mais uma");
+Console.Writeline ("Que hoje o rolê é bife e yung lixo");
+Console.Writeline ("Só get over here que isso te ajuda");
+Console.Writeline ("A ver a cena do trap caindo");
+Console.Writeline ("Drip veneno, desce mais um");
+Console.Writeline ("Que hoje o rolê é bife e yung lixo");
+Console.Writeline ("Só get over here que isso te ajuda");
+Console.Writeline ("A ver a cena do trap caindo");
+
+Console.Writeline ("Tô correndo aqui nas ruas da grande Acari");
+Console.Writeline ("Com um fusca rebaixado no estilo Bumblebee");
+Console.Writeline ("Tem uns cara atrás de mim tentando me impedir");
+Console.Writeline ("Quando eu entro na viela, eles não vêem pr'onde eu fugi");
+Console.Writeline ("Eles têm alcance, mano, porque eu sou o bicho");
+Console.Writeline ("Tu é Zeca Urubu, amigo, então 'cê fecha o bico");
+Console.Writeline ("Eu pego criminoso, apareci no Datena");
+Console.Writeline ("Com 100 mil no bolso, huh, Porsche Panamera");
+Console.Writeline ("Ninguém me aguenta, velocidade da luz");
+Console.Writeline ("Jogam até água benta, os cara já pegam a cruz");
+Console.Writeline ("Na curva, pneu cantando, mano, e tu me mamando");
+Console.Writeline ("Que nem Tá Dando Onda, eu sou o Cadu e tu é o Frango");
+Console.Writeline ("Nível da potência, não tem como se igualar");
+Console.Writeline ("Tamo' no pique mais alto que o próprio lobo-guará");
+Console.Writeline ("Na hora de fazer rima, mano, eu sou tenebroso");
+Console.Writeline ("Mando speed flow brabo, tipo XLR8");
+
+Console.Writeline ("Drip veneno, desce mais uma");
+Console.Writeline ("Que hoje o rolê é bife e yung lixo");
+Console.Writeline ("Só get over here que isso te ajuda");
+Console.Writeline ("A ver a cena do trap caindo");
+Console.Writeline ("Drip veneno, desce mais um");
+Console.Writeline ("Que hoje o rolê é bife e yung lixo");
+Console.Writeline ("Só get over here que isso te ajuda");
+Console.Writeline ("A ver a cena do trap caindo");
